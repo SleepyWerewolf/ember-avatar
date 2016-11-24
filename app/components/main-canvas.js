e724e87f8f3e5a38a1da6c006f19e71e0630cb0d
Viet Truong <viettruong2194@gmail.com>
2016-11-23 19:01:15 -0800

    Add sidebar toggle component, revert to not using pods, add Threejs and ember-browserify

---
+++ app/components/main-canvas.js
@@ -0,0 +1,3 @@
+import Three from 'npm:three';
+
+export { default } from 'ember-avatar/components/main-canvas';
--- app/components/main-canvas/component.js
+++
@@ -1,1 +0,0 @@
-export { default } from 'ember-avatar/components/main-canvas/component';
